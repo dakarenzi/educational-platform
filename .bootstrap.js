78248d03e47a9bfa726796f0825abc8d637ea0db
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-15 10:49:46 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun install react-dom-confetti"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
